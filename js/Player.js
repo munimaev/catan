fb4e64c59905b134f208270d8d3bdf2a4d5e7ae6
munimaev <munimaev@yandex.ru>
2014-12-03 17:25:10 +0300

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -4,74 +4,33 @@ var Player = function(args) {
         message: 'Недостаток аргументов color.'
       };
     }
-    if (!(args.hasOwnProperty('isYou'))) {
+    if (!(args.hasOwnProperty('role'))) {
       throw {
-        message: 'Недостаток аргументов isYou.'
+        message: 'Недостаток аргументов role.'
+      };
+    }
+    if (!(args.hasOwnProperty('color'))) {
+      throw {
+        message: 'Недостаток аргументов color.'
       };
     }
 	this.args = args;
-	this.isYou = args.isYou;
+  this.color = args.color;
+  this.$this = $('#' + args.role + 'Bar')
 	this.flags = {
 		createHTML : false,
 	};
 }
-Player.prototype.interfaceYour = $('.interface.your');
+Player.prototype.youBar  = $('#youBar');
+Player.prototype.opp1Bar = $('#opp1Bar');
+Player.prototype.opp2Bar = $('#opp2Bar');
+Player.prototype.opp3Bar = $('#opp3Bar');
 
 Player.prototype.createHTML = function() {
-  if (this.flags.createHTML) {
-    return;
-  }
-  var isYouOrOpp = this.isYou ? 'you' : 'opp';
-  var htmlClass = isYouOrOpp;
-  var $this = $('<div />', {
-    'class': htmlClass
-  });
-  $tr1 = $('<tr />');
-  $tr1.append($('<td />',{text:'build'}));
-  $tr1.append($('<td />',{text:'cost'}));
-  $tr1.append($('<td />',{text:' '}));
-  $tr1.append($('<td />',{text:'resourse'}));
-  $tr1.append($('<td />',{text:' '}));
-  $tr1.append($('<td />',{text:'card'}));
-  $tr1.append($('<td />',{text:' '}));
-  $tr1.append($('<td />',{text:'used'}));
-  $tr1.append($('<td />',{text:' '}));
-  $tr1.append($('<td />',{text:'points'}));
-
-  $tr2 = $('<tr />');
-  $tr2.append($('<td />',{text:'road'}));
-  $tr2.append($('<td />',{text:'[][]'}));
-  $tr2.append($('<td />',{rowspan:4}));
-  $tr2.append($('<td />',{rowspan:4}));
-  $tr2.append($('<td />',{rowspan:4}));
-  $tr2.append($('<td />',{rowspan:4}));
-  $tr2.append($('<td />',{rowspan:4}));
-  $tr2.append($('<td />',{rowspan:4}));
-  $tr2.append($('<td />',{rowspan:4}));
-  $tr2.append($('<td />',{rowspan:4}));
-
-  $tr3 = $('<tr />');
-  $tr3.append($('<td />',{text:'village'}));
-  $tr3.append($('<td />',{text:'[][][][]'}));
-
-
-  $tr4 = $('<tr />');
-  $tr4.append($('<td />',{text:'city'}));
-  $tr4.append($('<td />',{text:'[][][][][]'}));
-
-  $tr5 = $('<tr />');
-  $tr5.append($('<td />',{text:'card'}));
-  $tr5.append($('<td />',{text:'[][][]'}));
-
-  var $table = $('<table />',{cellspacing:'1px',cellpadding:0})
-  $table.append($tr1)
-  $table.append($tr2)
-  $table.append($tr3)
-  $table.append($tr4)
-  $table.append($tr5)
-
-  $this.append($table);
-
-  this.$this = $this;
-  this.flags.createHTML = true;
+  this.$this.addClass(this.color);
 }
+
+Player.prototype.updateHTML = function() {
+
+}
+
