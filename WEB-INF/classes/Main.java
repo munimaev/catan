b1284aa0fcd32ca0e5932a60817ec7364ac9f9ee
Kadegrov Viktor <kad-git@yandex.ru>
2014-12-05 14:33:06 +0300

    Добавлены плюшки для сборки на сервере

---
+++ WEB-INF/classes/Main.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Viktor on 05.12.2014.
+ */
+public class Main extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter pw = resp.getWriter();
+        pw.println("<h3>hello !</h3>");
+        pw.println("<h5>GET string:</h5>");
+        pw.println("<p>" +
+                req.getQueryString() +
+                "</p>");
+        pw.close();
+    }
+}
